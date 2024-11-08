a8c38eab5df971e42889e2a889caeeb7d7bb6d8c
Serhiy Zap <52965191+ReyterSS@users.noreply.github.com>
2024-11-08 22:20:45 +0200

    1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import csv
 import json
+import csv
+
 
 # function to get data from json file
 def load_data(filename):
@@ -11,60 +12,64 @@ def load_data(filename):
 def find_min_price(prices):
     min_value = float('inf')
     min_key = None
-    for i in prices:
-        shown_price = i['shown_price']
-        for key, value in shown_price.items():
-            value_float = float(value)
-            if value_float < min_value:
-                min_value = value_float
-                min_key = key
+    for price_info in prices:
+        for room_type, price in price_info['shown_price'].items():
+            price = float(price)
+            if price < min_value:
+                min_value = price
+                min_key = room_type
     return min_value, min_key
 
+
 # function to get taxes and number of guests
-def get_taxes_and_guests(number_of_quests):
-    for i in number_of_quests:
-        taxes = i["ext_data"]['taxes']
-        taxes = json.loads(taxes)
-        tax = taxes['TAX']
-        city_tax = taxes["City tax"]
-        number_of_guests = i['number_of_guests']
+def get_taxes_and_guests(data):
+    taxes = json.loads(data[0]["ext_data"]['taxes'])
+    tax = float(taxes['TAX'])
+    city_tax = float(taxes["City tax"])
+    number_of_guests = data[0]['number_of_guests']
     return tax, city_tax, number_of_guests
 
-# function to get price with taxes
+
+# function to get full price (net price + taxes) for each room
 def calculate_net_price(prices, tax, city_tax):
     result = []
-    for i in prices:
-        net_price = i["net_price"]
-        for key, value in net_price.items():
-            type_room = key
-            net_price_taxes = '{:.2f}'.format(float(value) + float(tax) + float(city_tax))
-            result.append({'Lowest price': type_room, 'Number of guests': net_price_taxes})
+    for price_info in prices:
+        for room_type, net_price in price_info["net_price"].items():
+            total_price = float(net_price) + tax + city_tax
+            result.append({'Room Type': room_type, 'Total Price': '{:.2f}'.format(total_price)})
     return result
 
+
 # function to write in csv file
-def write_to_file(data, filename='Result.txt'):
-    with open(filename, 'a', encoding='UTF-8') as f:
+def write_to_file(data, filename):
+    with open(filename, 'w', encoding='UTF-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=data[0].keys())
+        writer.writeheader()
         writer.writerows(data)
+
 
 # main function
 def main():
     # load data
     data = load_data('Python-task.json')
     prices = data['assignment_results']
-    number_of_quests = data['assignment_results']
+
     # get minimum price and type of room
     min_value, min_key = find_min_price(prices)
     print(f"Minimum price: {min_value}")
+    print(f"Room type with the minimum price: '{min_key}'")
+
     # getting taxes and number of guests
-    tax, city_tax, number_of_guests = get_taxes_and_guests(number_of_quests)
-    print(f"Type of room: '{min_key}', Number of guests: '{number_of_guests}'")
-    # getting price with taxes
+    tax, city_tax, number_of_guests = get_taxes_and_guests(prices)
+    print(f"Number of guests: {number_of_guests}")
+
+    # getting price with taxes for all rooms
     net_price_data = calculate_net_price(prices, tax, city_tax)
+
     # write to csv file
-    write_to_file(net_price_data)
+    output_filename = 'Result.csv'
+    write_to_file(net_price_data, output_filename)
+
 
 if __name__ == '__main__':
     main()
-
-
